fd8e2c3a859ab9c645f8768434694c8133188ae2
Merouane Grissen <megrisse@e3r7p10.1337.ma>
2023-07-26 20:37:32 +0100

    creat database and user in sql

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash 
 
-sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/bind-address            = 127.0.0.1/$db_user            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 service mysql start
 
 
-echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+echo "CREATE DATABASE $db_name ;" > database.sql
+echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password' ;" >> database.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> database.sql
+echo "ALTER USER '$db_user'@'%' IDENTIFIED BY '$db_password' ;" >> database.sql
+echo "FLUSH PRIVILEGES;" >> database.sql
 
-mysql < db1.sql
+mysql < database.sql
 
 sleep 100000
