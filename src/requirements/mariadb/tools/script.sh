e8097c7368c0c5d277edd7de32a5d189e8b927dc
Merouane Grissen <megrisse@e3r3p8.1337.ma>
2023-07-27 19:10:18 +0100

    fix probleme in mariadb script

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-sed -i "s/bind-address            = 127.0.0.1/$db_user            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -i "s/bind-address            = 127.0.0.1/bind-address           = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+
 
 service mysql start
-
 
 echo "CREATE DATABASE $db_name ;" > database.sql
 echo "CREATE USER '$db_user'@'%' IDENTIFIED BY '$db_password' ;" >> database.sql
@@ -13,4 +13,4 @@ echo "FLUSH PRIVILEGES;" >> database.sql
 
 mysql < database.sql
 
-sleep 100000
+mysqld
