9edd600004f5ab4206a1b52ae32f8d486972e730
Merouane Grissen <megrissen9@gmail.com>
2023-08-04 15:34:20 +0100

    finishing mandatory, init bonus part

--- src/requirements/nginx/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com"
-
-nginx -g "daemon off;"
