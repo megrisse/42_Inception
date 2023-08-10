e897ddbc4cb36e29d8d7a50cd3e0aa285456c058
Merouane Grissen <megrissen9@gmail.com>
2023-08-10 15:50:59 +0100

    changes in setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,2 +1,6 @@
+
+#rm -rf /home/megrisse/data
+#mkdir -vp /home/megrisse/data/mariadb /home/megrisse/data/wordpress
+
 rm -rf /Users/megrisse/Desktop/42_Inception/data
 mkdir -vp /Users/megrisse/Desktop/42_Inception/data/wordpress /Users/megrisse/Desktop/42_Inception/data/mariadb
