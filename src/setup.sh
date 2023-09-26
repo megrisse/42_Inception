9da69a4a3c03ceb5b95df4c3c45f9def4c8b4d5b
Merouane Grissen <megrissen9@gmail.com>
2023-09-26 22:49:23 +0100

    last version for vm...

--- src/setup.sh
+++ src/setup.sh
@@ -1,10 +1,3 @@
 
-# rm -rf /home/megrisse/data
-# mkdir -vp /home/megrisse/data/mariadb /home/megrisse/data/wordpress
-
-docker rm $(docker ps -qa)
-docker rmi $(docker images -q)
-docker volume rm $(docker volume ls -q)
-
-rm -rf /Users/megrisse/Desktop/42_Inception/data
-mkdir -vp /Users/megrisse/Desktop/42_Inception/data/wordpress /Users/megrisse/Desktop/42_Inception/data/mariadb
+rm -rf /home/megrisse/data
+mkdir -vp /home/megrisse/data/mariadb /home/megrisse/data/wordpress
